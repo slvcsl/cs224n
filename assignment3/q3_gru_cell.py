badd1de046f3e0bfacb77c64f97d8c4e30b3ec98
slvcsl <silvia.casola8@gmail.com>
2019-09-10 19:14:44 +0200

    gru cell finshed

--- assignment3/q3_gru_cell.py
+++ assignment3/q3_gru_cell.py
@@ -65,7 +65,19 @@ class GRUCell(tf.nn.rnn_cell.RNNCell):
         # be defined elsewhere!
         with tf.variable_scope(scope):
             ### YOUR CODE HERE (~20-30 lines)
-            pass
+            Uz = tf.get_variable("U_z", shape=(self.input_size, self.state_size), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+            Wz = tf.get_variable("W_z", shape=(self.state_size, self.state_size), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+            bz = tf.get_variable("b_z", shape=(self.state_size), dtype=tf.float32, initializer=tf.zeros_initializer())
+            z_t = tf.sigmoid(tf.matmul(inputs, Uz) + tf.matmul(state, Wz) + bz)
+            Ur = tf.get_variable("U_r", shape=(self.input_size, self.state_size), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+            Wr = tf.get_variable("W_r", shape=(self.state_size, self.state_size), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+            br = tf.get_variable("b_r", shape=(self.state_size), dtype=tf.float32, initializer=tf.zeros_initializer())
+            r_t = tf.sigmoid(tf.matmul(inputs, Ur) + tf.matmul(state, Wr) + br)
+            Uo = tf.get_variable("U_o", shape=(self.input_size, self.state_size), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+            Wo = tf.get_variable("W_o", shape=(self.state_size, self.state_size), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+            bo = tf.get_variable("b_o", shape=(self.state_size), dtype=tf.float32, initializer=tf.zeros_initializer())
+            o_t = tf.tanh(tf.matmul(inputs, Uo) + r_t * tf.matmul(state, Wo) + bo) 
+            new_state = z_t*state + (tf.ones_like(z_t)-z_t)*o_t
             ### END YOUR CODE ###
         # For a GRU, the output and state are the same (N.B. this isn't true
         # for an LSTM, though we aren't using one of those in our
